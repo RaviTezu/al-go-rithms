dd6dd8057273ce1e4fd9bd23c417b218c11952dc
RaviTezu <ravi-teja@live.com>
2018-04-18 01:17:40 +0530

    Adding Singly_Linked_List implementation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # al-go-rithms
 Minimal examples of data structures and algorithms in GoLang</br>
 Inspired by https://github.com/keon/algorithms 
+
+## List of Implementations
+- [linkedlist](linkedlist)
+
+## Tests
+To run the tests, clone the repo and run :  
+``` go test -v ./...```
--- cmd/linkedlist/main.go
+++ cmd/linkedlist/main.go
@@ -1,1 +1,36 @@
-package linkedlist
+package main
+
+import (
+	"github.com/ravitezu/al-go-rithms/linkedlist"
+)
+
+func main() {
+	sl := linkedlist.NewList()
+	for i := range "123456789"  {
+		sl.Append(i)
+	}
+	sl.Append("a")
+	sl.Append("b")
+	sl.Display()
+	sl.Reverse()
+	sl.Display()
+	sl.Append(-1)
+	sl.Display()
+	sl.Reverse()
+	sl.Display()
+	sl.Prepend(-2)
+	sl.Display()
+	n1 := linkedlist.NewNode("c")
+	sl.Tail.Next = n1
+	sl.Tail = n1
+	sl.Display()
+	n2 := linkedlist.NewNode("e")
+	sl.Tail.Next = n2
+	sl.Tail = n2
+	sl.Display()
+	err := sl.Insert(n1, "d")
+	if err != nil {
+		panic(err)
+	}
+	sl.Display()
+}
